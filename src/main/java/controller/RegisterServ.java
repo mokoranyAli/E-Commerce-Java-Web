3053fe35e2581ff10c3ba88496808a3b85c79bed
mokoranyAli <mohammed.korany.ali@gmail.com>
2019-02-16 20:21:23 +0200

    R

--- src/main/java/controller/RegisterServ.java
+++ src/main/java/controller/RegisterServ.java
@@ -15,6 +15,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.user;
 
 /**
  *
@@ -26,11 +27,15 @@ public class RegisterServ extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String username = request.getParameter("Username");
-        String Email = request.getParameter("Email");
+        user u = new user();
+             u.setSerial_number(request.getParameter("credit"));
+             u.setUser_email(request.getParameter("Email"));
+             u.setUser_password(request.getParameter("Password"));
+             u.setRole("User");
+             u.setUser_name(request.getParameter("credit"));
+             u.setSerial_number(request.getParameter("Username"));
+       
         String address = request.getParameter("address");
-        String serialNumber = request.getParameter("credit");
-        String password = request.getParameter("Password");
         String Confirm_Password = request.getParameter("Confirm_Password");
         String Role="User";
         Connection con = DatabaseConnection.getConnecttion();
@@ -40,16 +45,16 @@ public class RegisterServ extends HttpServlet {
         try {
             PreparedStatement pr =  con.prepareStatement(
                     "INSERT INTO `user`(`user_name`, `user_email`, `user_password`, `role`, `serial_number`) values (?,?,?,?,?)");
-            pr.setString(1, username);
-            pr.setString(2, Email);
-            pr.setString(3, password);
-            pr.setString(4, Role);
-            pr.setString(5, serialNumber);
+            pr.setString(1, u.getUser_name());
+            pr.setString(2, u.getUser_email());
+            pr.setString(3, u.getUser_password());
+            pr.setString(4, u.getRole());
+            pr.setString(5, u.getSerial_number());
             pr.executeUpdate();
             pr.close();
-          // response.sendRedirect("products.html"); 
-           request.setAttribute("username", username);
-           request.getRequestDispatcher("home.jsp").forward(request, response);
+           
+          request.setAttribute("user", u);         
+          request.getRequestDispatcher("home.jsp").forward(request, response);
         } catch (SQLException ex) {
         }
     }
